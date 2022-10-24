7c320e4ebddace9c4454e4648329f5453c9c4248
marijam25 <marijamilicevicc25@gmail.com>
2022-10-24 10:48:31 +0200

    Window commit

--- src/com/clv/Hello.java
+++ src/com/clv/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Maja");
+        System.out.println("window");
     }
 
 }
