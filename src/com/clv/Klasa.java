e6890ebb827217959407bbd26d498af8869cff4a
marijam25 <marijamilicevicc25@gmail.com>
2022-10-24 14:52:08 +0200

    Lorem ipsum

--- src/com/clv/Klasa.java
+++ src/com/clv/Klasa.java
@@ -3,5 +3,6 @@ package com.clv;
 public class Klasa {
     public static void main(String[] args) {
         System.out.println("Ovo je klasa");
+        System.out.println("lorem ipsum");
     }
 }
