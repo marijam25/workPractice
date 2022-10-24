b569bc5c868209c9749fb59f76c161fcb2a459dc
marijam25 <marijamilicevicc25@gmail.com>
2022-10-24 15:51:06 +0200

    Class2 created on branch2

--- src/com/clv/Hello.java
+++ src/com/clv/Hello.java
@@ -4,7 +4,7 @@ package com.clv;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Changed to Hi");
+        System.out.println("Hello");
         System.out.println("Maja");
         System.out.println("window");
     }
