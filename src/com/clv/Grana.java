a0666941be61f998ac9942eb81b00f65ead955a6
marijam25 <96152864+marijam25@users.noreply.github.com>
2022-10-25 10:38:46 +0200

    update grana

--- src/com/clv/Grana.java
+++ src/com/clv/Grana.java
@@ -3,5 +3,7 @@ package com.clv;
 public class Grana {
     public static void main(String[] args) {
         System.out.println("ovo je grana");
+        System.out.println("this is branch");
+        System.out.println("hello branch");
     }
 }
