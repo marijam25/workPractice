c9a66948c3ebcc853c1cac1fe6b778ac458ee2de
marijam25 <marijamilicevicc25@gmail.com>
2022-10-25 10:07:28 +0200

    update part1

--- src/com/clv/Part1.java
+++ src/com/clv/Part1.java
@@ -3,5 +3,7 @@ package com.clv;
 public class Part1 {
     public static void main(String[] args) {
         System.out.println("This is part1");
+        System.out.println("Hello everyone");
+        System.out.println("How are you");
     }
 }
