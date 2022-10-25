d78b14f111bd058c75c1e2ef54f87b50fc6e5bdf
marijam25 <marijamilicevicc25@gmail.com>
2022-10-25 10:31:30 +0200

    update part1 mate

--- src/com/clv/Part1.java
+++ src/com/clv/Part1.java
@@ -4,6 +4,6 @@ public class Part1 {
     public static void main(String[] args) {
         System.out.println("This is part1");
         System.out.println("Hello everyone");
-        System.out.println("How are you");
+        System.out.println("Hello mate, what's up???");
     }
 }
